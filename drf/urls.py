c269a9d6420ea1492a2fc1f4441f32b6c557ea57
Cristian Molina <megatux@gmail.com>
2023-12-27 16:09:29 -0300

    Move view & serializer outside urls.py

--- drf/urls.py
+++ drf/urls.py
@@ -1,36 +1,8 @@
-"""drf URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
-from django.contrib.auth.models import User
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+from drf.views import UserViewSet
 
 
 router = routers.DefaultRouter()
