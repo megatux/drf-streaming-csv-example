248f214c710cf6cd2474748fc6d5d7a73189644a
Cristian Molina <megatux@gmail.com>
2023-12-28 09:58:45 -0300

    Add streaming view

--- drf/tests.py
+++ drf/tests.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from drf.views import UserViewSet
 
 
 def test_user_list(django_user_model):
@@ -17,6 +18,41 @@ def test_user_list(django_user_model):
     response = client.get(list_url)
     assert response.status_code == status.HTTP_200_OK
     assert user_data['username'] in str(response.content)
+
+
+def test_user_list_streaming(django_user_model):
+    client = APIClient()
+    list_url = reverse('user-streaming_view')
+
+    users_data = [
+        {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+        },
+        {
+            'username': 'testuser2',
+            'email': 'testuser2@example.com',
+        }
+    ]
+    user1 = User.objects.create(**users_data[0])
+    user2 = User.objects.create(**users_data[1])
+
+    response = client.get(list_url)
+    assert response.status_code == status.HTTP_200_OK
+
+    # Read and decode the streamed content
+    streamed_content = b"".join(response.streaming_content).decode('utf-8')
+
+    # Assert the content is as expected
+    expected_content = users_data[0]['username'] + "," + \
+        users_data[0]['email'] + "," + str(user1.is_staff) + "\n"
+    assert expected_content in streamed_content
+
+    expected_content = users_data[1]['username'] + "," + \
+        users_data[1]['email'] + "," + str(user2.is_staff) + "\n"
+    assert expected_content in streamed_content
+
+    assert streamed_content.count("\n") == 2*UserViewSet.DUP_QUERYSET
 
 
 def test_new_user(django_user_model):
--- drf/views.py
+++ drf/views.py
@@ -1,13 +1,50 @@
-from rest_framework import routers, serializers, viewsets
+from django.http import StreamingHttpResponse
+from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
+from rest_framework.decorators import action
 
-# Serializers define the API representation.
+
+class QuerysetIterator:
+    def __init__(self, queryset, n):
+        self.queryset = queryset
+        self.n = n
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter < self.n:
+            self.counter += 1
+            return self.queryset.all()
+        else:
+            raise StopIteration
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
 
-# ViewSets define the view behavior.
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    DUP_QUERYSET = 30
+
+    def _item_to_csv(self, item):
+        return item.username + "," + item.email + "," + str(item.is_staff) + "\n"
+
+    def stream_data(self, queryset):
+        iterator = QuerysetIterator(queryset, self.DUP_QUERYSET)
+        for items in iterator:
+            for item in items:
+                yield str(self._item_to_csv(item))
+
+    @action(methods=["GET"], detail=False, url_path="streaming_view", url_name="streaming_view",)
+    def streaming_view(self, request):
+        queryset = User.objects.all()
+
+        response = StreamingHttpResponse(
+            self.stream_data(queryset), content_type="text/csv")
+        return response
